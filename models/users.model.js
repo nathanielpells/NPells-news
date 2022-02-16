ca7e8b4e19572ba7e0bf844bb4ee8cec371460cd
Nathaniel Pells <nathaniel.96.np@gmail.com>
2022-02-16 11:50:42 +0000

    removed console log ready to commit to main

--- models/users.model.js
+++ models/users.model.js
@@ -2,7 +2,6 @@ const db = require("../db/connection");
 
 exports.fetchUsers = () => {
   return db.query("SELECT username FROM users;").then((result) => {
-    console.log(result.rows);
     return result.rows;
   });
 };
