5c24893e62139139b5e561e6394d469f63d9963f
Nathaniel Pells <nathaniel.96.np@gmail.com>
2022-02-16 12:31:31 +0000

    changed the order of the error controllers

--- controllers/errors.controller.js
+++ controllers/errors.controller.js
@@ -2,13 +2,13 @@ exports.handle404 = (req, res) => {
   res.status(404).send({ msg: "page not found" });
 };
 
-exports.handle500 = (err, req, res) => {
-  res.status(500).send({ msg: "internal server error" });
-};
-
 exports.handlePsqlError = (err, req, res, next) => {
   console.log(err.code);
   if (err.code === "22P02")
     res.status(400).send({ msg: "invalid data type(s) given" });
   else next(err);
+};
+
+exports.handle500 = (err, req, res) => {
+  res.status(500).send({ msg: "internal server error" });
 };
