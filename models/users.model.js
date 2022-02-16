1f5c8df181b93d0774efe414b37d422260687efd
Nathaniel Pells <nathaniel.96.np@gmail.com>
2022-02-16 10:41:08 +0000

    added a console log to make my main branch differ from this branch

--- models/users.model.js
+++ models/users.model.js
@@ -2,6 +2,7 @@ const db = require("../db/connection");
 
 exports.fetchUsers = () => {
   return db.query("SELECT username FROM users;").then((result) => {
+    console.log(result.rows);
     return result.rows;
   });
 };
