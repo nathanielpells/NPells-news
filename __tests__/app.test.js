2c3469d93c458981cd10f5fed48a38ae2120ed67
Nathaniel Pells <nathaniel.96.np@gmail.com>
2022-02-18 15:30:23 +0000

    cant pass first test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -272,7 +272,7 @@ describe("GET - /api/topics", () => {
         });
     });
   });
-  describe.only("GET - /api/articles", () => {
+  describe("GET - /api/articles", () => {
     test("status 200 - responds with all of the articles with a comment count property added to them with the correct number of comments in each.", () => {
       return request(app)
         .get("/api/articles")
@@ -294,13 +294,13 @@ describe("GET - /api/topics", () => {
           });
         });
     });
-    //   test("Status: 200 - should respond with an array of article objects sorted by user's choice", () => {
-    //     return request(app)
-    //       .get("/api/articles?sort_by=votes")
-    //       .expect(200)
-    //       .then(({ body: articles }) => {
-    //         expect(articles).toBeSortedBy("votes", { descending: true });
-    //       });
-    //   });
+    // test("Status: 200 - should respond with an array of article objects sorted by user's choice", () => {
+    //   return request(app)
+    //     .get("/api/articles?sort_by=votes")
+    //     .expect(200)
+    //     .then(({ body: articles }) => {
+    //       expect(articles).toBeSortedBy("votes", { descending: true });
+    //     });
+    // });
   });
 });
